12cbc493f10512b8060b0beb5f2b97ec6e380624
Kelly Smith <kssmith92@gmail.com>
2018-10-25 20:18:59 -0400

    commit hw file

---
+++ hw.js
@@ -0,0 +1,24 @@
+// FORLOOPS HW
+
+for (let index = 0; index < array.length; index++) {   
+}
+// the following line is the same as line 38, just shorthand
+array.forEach(function(item){
+
+});
+
+for (const key in object) {
+
+}
+
+for (const iterator of object) {
+
+}
+
+// while (condition) {
+    
+// }
+
+// do {
+    
+// } while (condition);
