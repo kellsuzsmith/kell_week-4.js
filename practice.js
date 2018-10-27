4262f1b92c431753497205b71a81369d1046a042
Kelly Smith <kssmith92@gmail.com>
2018-10-27 14:06:52 -0400

    practice commit

---
+++ practice.js
@@ -0,0 +1,10 @@
+const myList = [
+    {person: 1, name: "erik", age: 30},
+    {person: 2, name: "wendy", age: 29},
+    {person: 3, name: "jack", age: 29},
+    {person: 4, name: "kelly", age: 26},
+];
+var personNames = myList.map(function (name) {
+    return list.name;
+});
+
